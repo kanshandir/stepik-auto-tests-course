c94bb801d114c423f9fb5ec1fc616f388706ce21
Kantemir <kan.shandir@gmail.com>
2021-01-19 21:44:48 +0300

    modified test and commit gitignore

--- test_abs_project.py
+++ test_abs_project.py
@@ -1,12 +1,61 @@
 import unittest
+from selenium import webdriver
 
 
 class TestAbs(unittest.TestCase):
     def test_abs1(self):
-        self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        input1 = browser.find_element_by_xpath("//input[@placeholder='Input your first name']")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element_by_xpath("//input[@placeholder='Input your last name']")
+        input2.send_keys("Ivanov")
+        input3 = browser.find_element_by_xpath("//input[@placeholder='Input your email']")
+        input3.send_keys("ivan07@list.ru")
+        input3 = browser.find_element_by_xpath("//input[@placeholder='Input your phone:']")
+        input3.send_keys("11222344455")
+        input3 = browser.find_element_by_xpath("//input[@placeholder='Input your address:']")
+        input3.send_keys("Moscow")
+
+        # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text,
+                         "Texts aren't equal")
 
     def test_abs2(self):
-        self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        input1 = browser.find_element_by_xpath("//input[@placeholder='Input your first name']")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element_by_xpath("//input[@placeholder='Input your email']")
+        input2.send_keys("ivan07@list.ru")
+
+        # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text,
+                         "Texts aren't equal")
 
 
 if __name__ == "__main__":
